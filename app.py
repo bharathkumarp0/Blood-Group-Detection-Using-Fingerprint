881de6bef3559cb29b79da36fd0c64bf795b3e8b
bharathkumarp0 <bharathkumarp0058@gmail.com>
2025-01-31 09:55:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -82,5 +82,7 @@ def download_dataset():
     except Exception as e:
         return jsonify({'success': False, 'message': str(e)}), 500
 
+
 if __name__ == '__main__':
-    app.run(debug=True) 
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(host='0.0.0.0', port=port)
